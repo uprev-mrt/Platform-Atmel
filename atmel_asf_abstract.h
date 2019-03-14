0539dfd208c8f88aae203d06833d8f69cc4befab
Jason Berger <berge472@gmail.com>
2019-03-14 01:32:30 -0400

    removed parallel and switched to gpio_port write/read

--- atmel_asf_abstract.h
+++ atmel_asf_abstract.h
@@ -32,7 +32,9 @@
 
 /************* GPIO Abstraction ******************/
 typedef uint8_t mrt_gpio_t;
+typedef PortGroup* mrt_gpio_port_t;
 #define MRT_GPIO_WRITE(pin,val) port_pin_set_output_level(pin,val)
+#define MRT_GPIO_PORT_WRITE(port, mask, val) port_group_set_output_level(port, mask, val)
 #define MRT_GPIO_READ(pin) port_pin_get_output_level(pin)
 
 /************* I2C Abstraction ******************/
@@ -45,39 +47,6 @@ typedef uint8_t mrt_gpio_t;
 /************* SPI Abstraction ******************/
 // typedef spi_m_sync_descriptor* mrt_spi_handle_t;
 // #define MRT_SPI_TRANSFER(handle ,tx, rx ,len, timeout)
-
-
-/************* Parallel Abstraction ******************/
-typedef struct{
-  PortGroup* mDataPort;	//port for bus
-  uint32_t	mPortMask;			//mask for port
-  uint8_t mDataOffset;			//offset of data pins
-  uint32_t mWR;					//WR pin mask
-  uint32_t mRD;					//RD pin mask
-  uint32_t mA0;					//AO pin mask
-  uint8_t mDir;					//direction config
-}mrt_parallel_bus_t;
-typedef mrt_parallel_bus_t* mrt_pbus_handle_t;
-
-/**
-  *@brief set A0 bit on bus based on desired mode
-  *@param handle ptr to pbus handle
-  *@pre cmdMode True for command mode, False for data mode
-  */
-#define MRT_PBUS_MODE(handle, mode)											\
-if(mode == PBUS_MODE_CMD)														\
-	port_group_set_output_level(handle->mDataPort, handle->mA0, 0 );			\
-else																			\
-	port_group_set_output_level(handle->mDataPort, handle->mA0, handle->mA0 )
-
-
-#define MRT_PBUS_WRITE(handle, data)																							\
-		port_group_set_output_level(handle->mDataPort, handle->mPortMask, (data << handle->mDataOffset) | handle->mRD );		\
-		MRT_DELAY_US(1);																										\
-		port_group_set_output_level(handle->mDataPort, handle->mWR, handle->mWR)
-
-//#define MRT_PBUS_READ(handle, data, len)
-
 
 /************* PRINTF Abstraction ******************/
 #define MRT_PRINTF(f_, ...) printf((f_), __VA_ARGS__)
