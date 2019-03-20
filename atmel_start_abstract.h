f57eb90848242d3323c28984871d464bb94d1c7b
Jason Berger <berge472@gmail.com>
2019-03-20 14:58:44 -0400

    adding port write to atmel start

--- atmel_start_abstract.h
+++ atmel_start_abstract.h
@@ -7,40 +7,34 @@
 
 #pragma once
 
-#ifdef __cplusplus
- extern "C" {
-#endif
-
 #include <atmel_start.h>
 
-#ifdef __cplusplus
- }
- namespace Platforms
- {
-#endif
-
-typedef uint32_t mrt_status_t;
 
 //Delay Abstraction
 #define MRT_DELAY_MS(ms) delay_ms(ms)
 
 //Uart Abstraction
-typedef io_descriptor* mrt_uart_handle_t;
+/*typedef io_descriptor* mrt_uart_handle_t;
 #define MRT_UART_TX(handle, data, len, timeout) io_write(handle, data, len)
-#define MRT_UART_RX(handle, data, len, timeout) io_read(handle, data, len)
+#define MRT_UART_RX(handle, data, len, timeout) io_read(handle, data, len)*/
 
 //GPIO Abstraction
 typedef uint8_t mrt_gpio_t;
+typedef enum gpio_port mrt_gpio_port_t;
 #define MRT_GPIO_WRITE(pin,val) gpio_set_pin_level(pin,val)
 #define MRT_GPIO_READ(pin) gpio_get_pin_level(pin)
+#define MRT_GPIO_PORT_WRITE(port, mask, val) gpio_set_port_level(port, mask, val)
+#define MRT_GPIO_PORT_READ(port) gpio_get_port_level(port)
 
-//I2C Abstraction
-typedef i2c_m_sync_desc* mrt_i2c_handle_t;
-typedef int32_t mrt_i2c_status_t;
-#define MRT_I2C_MASTER_TRANSMIT(handle ,addr,data,len, stop, timeout)
-#define MRT_I2C_MASTER_RECEIVE(handle ,addr, data, len, stop, timeout)
-#define MRT_I2C_MEM_WRITE(handle, addr, mem_addr, mem_size, data, len, timeout ) i2c_m_sync_cmd_write(handle, mem_addr, data, len)
-#define MRT_I2C_MEM_READ(handle, addr, mem_addr, mem_size, data, len, timeout ) i2c_m_sync_cmd_read(handle, mem_addr, data, len)
+/*//I2C Abstraction
+#ifdef i2c_m_sync_desc
+	typedef i2c_m_sync_desc* mrt_i2c_handle_t;
+	typedef int32_t mrt_i2c_status_t;
+	#define MRT_I2C_MASTER_TRANSMIT(handle ,addr,data,len, stop, timeout)
+	#define MRT_I2C_MASTER_RECEIVE(handle ,addr, data, len, stop, timeout)
+	#define MRT_I2C_MEM_WRITE(handle, addr, mem_addr, mem_size, data, len, timeout ) i2c_m_sync_cmd_write(handle, mem_addr, data, len)
+	#define MRT_I2C_MEM_READ(handle, addr, mem_addr, mem_size, data, len, timeout ) i2c_m_sync_cmd_read(handle, mem_addr, data, len)
+#endif
 
 //SPI Abstraction
 typedef spi_m_sync_descriptor* mrt_spi_handle_t;
@@ -52,16 +46,9 @@ typedef int32_t mrt_spi_status_t;
                           .size = len,                       \
                           };                                 \
                           spi_m_sync_transfer(handle, &xfer);
-
-//8080 Parallel abstraction
-/*typedef int mrt_8080_handle_t;
-#define MRT_8080_CMD(handle, cmd)
-#define MRT_8080_DATA_READ(handle, data, len)
-#define MRT_8080_DATA_WRITE(handle, data, len)*/
+*/
 
 //printf
 #define MRT_PRINTF(f_, ...) printf((f_), __VA_ARGS__)
 
-#ifdef __cplusplus
- }
-#endif
+
